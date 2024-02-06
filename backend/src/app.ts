1d0cc48720ac3708b227bd68403f8a5e58416ab2
José Eduardo Rocha Peres <joseeduardorperes@gmail.com>
2024-02-06 18:32:28 -0300

    [BackEnd] feat: adicionando projeto back-end

---
+++ backend/src/app.ts
@@ -0,0 +1,53 @@
+import http from 'node:http';
+import express, { Application } from 'express';
+import { Server, Socket } from 'socket.io';
+
+class App {
+	private app: Application;
+	private http: http.Server;
+	private io: Server;
+
+	constructor() {
+		this.app = express();
+		this.http = new http.Server(this.app);
+		this.io = new Server(this.http, {
+			cors: {
+				origin: '*',
+			},
+		});
+	}
+
+	public listen() {
+		this.http.listen(3333, () => {
+			console.log('Server running on port 3333');
+		});
+	}
+
+	public listenSocket() {
+		this.io.of('/streams').on('connection', this.socketEvents);
+	}
+
+	private socketEvents(socket: Socket) {
+		console.log('socket conected:', socket.id);
+
+		socket.on('subscribe', (data) => {
+			console.log('Usuário inserido na sala:', data.roomId);
+
+			socket.join(data.roomId);
+
+			socket.on('chat', (data) => {
+				console.log('data:', data);
+
+				socket.broadcast.to(data.roomId).emit('chat', {
+					message: data.message,
+					username: data.username,
+					time: data.time,
+				});
+
+				return null;
+			});
+		});
+	}
+}
+
+export { App };
